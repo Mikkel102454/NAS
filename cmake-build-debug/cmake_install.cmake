71295b8a20dfd29f909daf41cc165f5ee79aced4
mikkel102454 <mikkeliversen09@outlook.dk>
2025-01-27 22:04:48 +0100

    started with socket creation

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Mikkel/CLionProjects/NAS
+# Install script for directory: C:/Users/mikke/CLionProjects/NAS
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Mikkel/CLionProjects/NAS/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/mikke/CLionProjects/NAS/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <winsock2.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    int server_fd;
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(server_fd == -1){
+        perror("Failed to create socket");
+        exit(-1);
+    }
+
     return 0;
 }
-e
